be18e2667731dc137cb7b8479f347c39fa87d344
Jessica Salbert <jessicarsalbert@gmail.com>
2020-10-05 03:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,11 @@
 // Write your code here
 const num1 = 31;
 const num2 = 2;
-function multiply() {
-  return num1 * num2;
-}
+const multipl = num1 * num2;
 
-function random() {
-  return Math.floor(Math.random() * 10) + 1;
-}
+const random = Math.floor(Math.random() * 10) + 1;
 
-function mod() {
-  return 4 % 5;
-}
 
-function max() {
-  return Math.max(1, 2, 20)
-}
+const mod = 4 % 5;
+
+const max = Math.max(1, 2, 20)
